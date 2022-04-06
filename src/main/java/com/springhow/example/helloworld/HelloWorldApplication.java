15e209db2276f8c591de854325b55aba4e5247ab
Sheraj Ahamad <94781120+sheraj2939@users.noreply.github.com>
2022-04-06 17:23:57 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,27 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "complete jenkins project ";
+        return "/usr/bin/which: no tomcat in (/opt/maven/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin)
+[root@localhost /]# ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host
+       valid_lft forever preferred_lft forever
+2: enp0s3: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:1c:7f:ad brd ff:ff:ff:ff:ff:ff
+    inet 192.168.11.205/23 brd 192.168.11.255 scope global noprefixroute dynamic enp0s3
+       valid_lft 6708sec preferred_lft 6708sec
+    inet6 fe80::de35:c016:61ff:c4f7/64 scope link noprefixroute
+       valid_lft forever preferred_lft forever
+3: virbr0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default qlen 1000
+    link/ether 52:54:00:bc:c2:d6 brd ff:ff:ff:ff:ff:ff
+    inet 192.168.122.1/24 brd 192.168.122.255 scope global virbr0
+       valid_lft forever preferred_lft forever
+4: virbr0-nic: <BROADCAST,MULTICAST> mtu 1500 qdisc pfifo_fast master virbr0 state DOWN group default qlen 1000
+    link/ether 52:54:00:bc:c2:d6 brd ff:ff:ff:ff:ff:ff
+";
     }
 
 }
