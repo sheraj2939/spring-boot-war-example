592d26b7daa8763020af7fe0978a6ecfcd09c8dc
Sheraj Ahamad <94781120+sheraj2939@users.noreply.github.com>
2022-04-06 17:37:14 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,35 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "1. Install tomcat package from repository by yum command.
-# yum install tomcat
-2. Edit tomcat.conf file and append below line there by using vi editor.
-# vim /usr/share/tomcat/conf/tomcat.conf
-JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx1026m 
--XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"
-3. Install all other tomcat packages like tomcat webapps, tomcat docs etc.
-# yum install tomcat*
-4. Edit tomcat-user.xml file and write below (use less than symbol before starting this line and greater 
-than symbol after) line there in tomcat-users section by vi editor, save it.
-# vim /usr/share/tomcat/conf/tomcat-users.xml
-user username="admin" password="password" roles="manager-gui,admin-gui"/
-5. Start tomcat service now.
-# systemctl start tomcat
-6. Set tomcat service to start automatically after reboot.
-# systemctl enable tomcat
-7. Add port 8080 (to be used by tomcat) in firewall.
-# firewall-cmd --permanent --zone=public --add-port=8080/tcp
-8. Reload the firewall.
-# firewall-cmd --reload
-Now Test your configuration:
-Open Firefox and write below line in address bar.
-localhost:8080
-or
-Open any browser and write below line in address bar.
-ip-address:8080
-(where ip-address is the IP address of your tomcat server)
-You should see a webpage showing successful configuration of Tomcat Apache.
-You have successfully installed and configured Tomcat Apache in Linux";
+        return "You have successfully installed and configured Tomcat Apache in Linux";
     }
 
 }
